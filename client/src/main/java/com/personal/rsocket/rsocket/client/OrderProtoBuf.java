5c709bf8ed9dbefed36f52fe391ab3443206ab5f
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2021-01-05 16:30:40 -0300

    Add REST endpoint upon protobuf
    - Add endpoint to consume downstream using protobuf
      the impl is on client side and server side is on wip

---
+++ client/src/main/java/com/personal/rsocket/rsocket/client/OrderProtoBuf.java
@@ -0,0 +1,46 @@
+package com.personal.rsocket.rsocket.client;
+
+import com.personal.rsocket.rsocket.data.Order;
+import lombok.AllArgsConstructor;
+import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.protobuf.ProtobufDecoder;
+import org.springframework.http.codec.protobuf.ProtobufEncoder;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@RestController
+public class OrderProtoBuf {
+    private final RSocketRequester requesterBuilder;
+
+    @Autowired
+    public OrderProtoBuf(RSocketRequester.Builder builder) {
+        requesterBuilder = builder
+                .rsocketStrategies(it -> {
+                    it.encoder(new ProtobufEncoder())
+                            .decoder(new ProtobufDecoder())
+                            .build();
+                })
+                .setupRoute("protobuf-client")
+                .setupData(UUID.randomUUID().toString())
+                .tcp("localhost", 7000);
+    }
+
+    @GetMapping(
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE,
+            value = "/order/protobuf/stream/monitor/{id}"
+    )
+    public Publisher<Order> orderStream(@PathVariable String id) {
+
+        return this.requesterBuilder
+                .route("monitor-protobuf")
+                .data(id)
+                .retrieveFlux(Order.class);
+    }
+}
