c6ce3dad8359bdaa8feaa080470025ea797d5921
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2021-01-05 16:29:13 -0300

    Fix type in class name

--- client/src/main/java/com/personal/rsocket/rsocket/RSocketApplication.java
+++ client/src/main/java/com/personal/rsocket/rsocket/RSocketApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RsocketApplication
+public class RSocketApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(RsocketApplication.class, args);
+        SpringApplication.run(RSocketApplication.class, args);
     }
 }
